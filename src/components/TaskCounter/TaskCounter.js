4f957bef3137527110a68c10e62237d5f234f050
Yaroslav Protsiuk <jaroslav.procjuk@gmail.com>
2023-07-18 22:44:56 +0300

    виконано з редакс

---
+++ src/components/TaskCounter/TaskCounter.js
@@ -0,0 +1,28 @@
+// Імпортуємо хук
+import { useSelector } from 'react-redux';
+import { getTasks } from '../../redux/selectors';
+
+export const TaskCounter = () => {
+  // Отримуємо масив завдань із стану Redux
+  const tasks = useSelector(getTasks);
+
+  // На базі стану Redux отримуємо похідні дані
+  const count = tasks.reduce(
+    (acc, task) => {
+      if (task.completed) {
+        acc.completed += 1;
+      } else {
+        acc.active += 1;
+      }
+      return acc;
+    },
+    { active: 0, completed: 0 }
+  );
+
+  return (
+    <div>
+      <p>Active: {count.active}</p>
+      <p>Completed: {count.completed}</p>
+    </div>
+  );
+};
---
+++ src/components/TaskForm/TaskForm.js
@@ -0,0 +1,25 @@
+// Імпортуємо хук
+import { useDispatch } from 'react-redux';
+// Імпортуємо генератор екшену
+import { addTask } from '../../redux/actions';
+
+export const TaskForm = () => {
+  // Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+    // Викликаємо генератор екшену та передаємо текст завдання для поля payload
+    // Відправляємо результат – екшен створення завдання
+    dispatch(addTask(form.elements.text.value));
+    form.reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="text" placeholder="Enter task text..." />
+      <button type="submit">Add task</button>
+    </form>
+  );
+};
